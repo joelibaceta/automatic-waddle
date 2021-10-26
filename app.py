6d85a0ed1b74fb02c2596811ffc63b2ccdc25d3e
Joel Ibaceta <mail@joelibaceta.com>
2021-10-25 22:32:10 -0500

    update

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "hello world"
+    return "hola mundo"
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
